627381031c16c7b2ef1729dd7809222a61137d6f
Septhianto Diga & Fadhli Ramadhani Arislan <pair+digachandra.fadhlirams@hacktiv8.com>
2016-08-31 15:22:07 +0700

    Nested Array 2

--- nested_array_2.js
+++ nested_array_2.js
@@ -5,11 +5,48 @@
 function tic_tac_toe_board() {
   var xo_random = ["X","O","X","O","X","O","X","O","X"]
   //write your code here
+  var tic_tac_toe = [];
+  var random_idx = 0;
 
+  // Function to check value tic_tac_toe - start
+  let x_total = o_total = 0;
+  // line to check xo_random length exactly 9 or not
+  if(xo_random.length != 9) return "Tic Tac Toe Error #1";
+
+  // line to check xo_random value 'x' and '0' have the same count / difference
+  for(let x = 0; x < xo_random.length; x++){
+    if(xo_random[x] == "X") x_total += 1;
+    else if(xo_random[x] == "O") o_total += 1;
+  }
+  if(!((x_total == 5 && o_total == 4) || (x_total == 4 && o_total == 5))) return "Tic Tac Toe Error #2";
+  // Function to check value tic_tac_toe - end
+
+  // Function to suffle array - start
+  function suffle_array(){
+    for(let x = 0; x < xo_random.length; x++){
+      // line code to get random position
+      let temp_index = Math.floor((Math.random() * xo_random.length));
+
+      // line code to swap value
+      let temp_value = xo_random[temp_index];
+      xo_random[temp_index] = xo_random[x];
+      xo_random[x] = temp_value;
+    }
+  }
+  suffle_array();
+  // Function to suffle array - end
+
+  for(let x = 0; x < 3; x++){
+    tic_tac_toe[x] = [];
+    for(let y = 0; y < 3; y++){
+      tic_tac_toe[x][y] = xo_random[random_idx];
+      random_idx += 1;
+    }
+  }
+  return tic_tac_toe;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
-
 
 // ---- RELEASE 1 ---------
 var roster = [["Number", "Name", "Position", "Points per Game"],
@@ -26,9 +63,17 @@ var roster = [["Number", "Name", "Position", "Points per Game"],
 
 function convert_roster_format(nestedArray) {
   // your convert code here
-
+  let object_roster = [];
+  let object_index = 0;
+  for(let x = 1; x < nestedArray.length; x++){
+    object_roster[object_index] = {};
+    for(let y = 0; y < nestedArray[x].length; y++){
+      object_roster[object_index][nestedArray[0][y]] = nestedArray[x][y];
+    }
+    object_index += 1;
+  }
+  return object_roster;
 }
-
 
 var object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
